760a65e5e026e2cbb56e713c5129348dcd44cde7
Mathias Lund Ahrn <golvkaldt@gmail.com>
2022-07-28 23:56:47 +0200

    Writeup doc

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repository = "fem.product-preview-card";
+
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const baseroute =
-    process.env.NODE_ENV === "production" ? "/frontendmentor/" : "/";
+  const baseRoute = process.env.NODE_ENV === "production" ? repository : "/";
   // https://vitejs.dev/config/
   return defineConfig({
-    base: baseroute,
+    base: baseRoute,
     plugins: [react()],
   });
 };
