74e852605c1bd24a1e6875e1803625d5d1862528
Mathias Lund Ahrn <golvkaldt@gmail.com>
2022-07-29 00:03:30 +0200

    Fixup routename in vite config

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:certinax/frontendmentor.git main:gh-pages
+git push -f git@github.com:certinax/fem.product-preview-card.git main:gh-pages
 
 cd -
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repository = "fem.product-preview-card";
+const repository = "/fem.product-preview-card/";
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
